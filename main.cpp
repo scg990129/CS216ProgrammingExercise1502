76b14947a71e2a8115467998490fc3dbe843c7ef
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-27 21:42:46 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>     // std::cout, std::endl
+#include <iomanip>      // std::setw
+
+using namespace std;
+
+void printRow(const int& , int );
+
+int main() {
+    int rowNumber;
+    cin >> rowNumber;
+    cout << endl;
+    printRow(rowNumber, 1);
+
+    return 0;
+}
+
+void printRow(const int& rowNumber, int currentRow){
+    if(currentRow >= rowNumber*2){
+        return;
+    }
+
+    cout << setw(abs(rowNumber - currentRow)+2);
+    for(int i = 0; i < rowNumber - abs(rowNumber - currentRow); i++)
+        cout << "* ";
+//    cout << abs(rowNumber - currentRow)
+    cout << endl;
+
+    printRow(rowNumber, currentRow + 1);
+}
